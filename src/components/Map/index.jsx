0c5406e5458e928c98d921d75c70b1d7a1e7803b
Maxime BOUDIER <mboudier-prestataire@vente-privee.com>
2018-05-16 14:27:47 +0200

    add map

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import injectSheet from 'react-jss'
+import { GoogleMap, Marker, withGoogleMap, withScriptjs } from "react-google-maps"
 
 const styles = {
 
 }
 
-const Map = ({ classes }) => (
-  <div>Map</div>
+const Map = ({ markers = [], classes }) => (
+  <GoogleMap
+    defaultZoom={4}
+    defaultCenter={{ lat: 45.954976, lng: -10.501562 }}
+  >
+    { markers && markers.map(() => (<Marker position={{ lat: -34.397, lng: 150.644 }} />)) }
+  </GoogleMap>
 )
 
-export default injectSheet(styles)(Map)
+export default injectSheet(styles)(withScriptjs(withGoogleMap(Map)))
