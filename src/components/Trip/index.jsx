dd9122901c701f5de0c02fabbc30e55940e0dda8
Marc Fagot <mfagot@vente-privee.com>
2018-05-17 14:26:21 +0200

    Trips updated

--- src/components/Trip/index.jsx
+++ src/components/Trip/index.jsx
@@ -60,25 +60,91 @@ const ratingImgs = ( stars ) => {
   />)
 }
 
+const listDepaturesCities = departures => {
+  const allDepartureCities = departures.reduce((allCities, departure) => [...allCities, ...departure.departureCities], []);
+  const listCities = [...new Set(allDepartureCities)];
+  let listCitiesString;
+
+  if (listCities.length <= 3) {
+    listCitiesString = listCities.reduce((str, city) => str + ", " + city, "");
+  } else {
+    const tmpStr = listCities
+      .slice(0, 3)
+      .reduce((str, city) => str + ", " + city, "");
+    listCitiesString = tmpStr + "...";
+  }
+
+  return listCitiesString.substr(2);
+};
+
+const listTripActivities = trip => {
+  let activitiesStr
+
+  if (trip.activities.length <= 3) {
+    activitiesStr = trip.activities.reduce((str, activity) => str + ", " + activity, "")
+  } else {
+    activitiesStr = trip.activities.slice(0, 3).reduce((str, activity) => str + ", " + activity, "")
+    activitiesStr += "..."
+  }
+
+  return activitiesStr.substr(2)
+};
+  
+const weatherLabel = departures => departures[0].climate
+
+const temperatureLabel = departures => {
+  return departures[0].temperature.toString() + " °C"
+}
+
+const saisonalityLabel = departures => {
+  return departures[0].saisonality + " saison"
+}
+
+const securityBlock = () => {
+  return (
+    <div>
+      <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/SecuriteRose.png`} />
+      <div> Pays sûr </div>
+    </div>
+  )
+}
+
 const Trip = ({ trip: { offerDescription: trip, departureDates: depatures }, classes }) => (
   <div className={classes.wrapper}>
 
     <div className={classes.product}>
 
       <div class={classes.productLeft}>
-        <p style={{fontWeight: "bold", fontSize: "16px"}}>{trip.label} {ratingImgs(trip.stars)}</p>
+        <p style={{ fontWeight: "bold", fontSize: "16px" }}>{trip.label} {ratingImgs(trip.stars)}</p>
         <p>
           <img src={`${process.env.PUBLIC_URL}/img/product/PinLocation.png`} width="15px" height="15px" />
-          <span style={{fontWeight: "bold", color: "#B7B7B7"}}> {trip.address}</span> <strong>{trip.country}</strong>
+          <span style={{ fontWeight: "bold", color: "#B7B7B7" }}> {trip.address}</span> <strong>{trip.country}</strong>
         </p>
         <div>
           <p className={classes.pictures}><img src={trip.image} className={classes.picture} alt={trip.label} /></p>
           <div className={classes.description}>
             <p>{trip.description}</p>
             <div>
-              <img src={`${process.env.PUBLIC_URL}/img/filters/VilleDepartRose.png`} />
-              <div> { [...new Set(depatures.reduce((acc, curr) => acc + curr.departureCities, ''))] } </div>
+              <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/VilleDepartRose.png`} />
+              <div> {listDepaturesCities(depatures)} </div>
             </div>
+            <div>
+              <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/ActivitesRose.png`} />
+              <div> {listTripActivities(trip)} </div>
+            </div>
+            <div>
+              <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/ClimatRose.png`} />
+              <div> {weatherLabel(depatures)} </div>
+            </div>
+            <div>
+              <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/TemperatureRose.png`} />
+              <div> {temperatureLabel(depatures)} </div>
+            </div>
+            <div>
+              <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/SaisonnaliteRose.png`} />
+              <div> {saisonalityLabel(depatures)} </div>
+            </div>
+            {trip.security && securityBlock()}
           </div>
         </div>
       </div>
@@ -87,7 +153,7 @@ const Trip = ({ trip: { offerDescription: trip, departureDates: depatures }, cla
         <p class={classes.price}>À partir de {Math.min.apply(Math, depatures.map(depature => depature.price))}€/pers</p>
         <div className={classes.tripAdvisor}>
           <p>Note <strong>TripAdvisor</strong></p>
-          <div style={{display : 'none'}}>
+          <div>
             <img
               src={`http://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/${trip.tripadvisor.rating.toFixed(1)}-MCID-5.png`}
               alt='tripadvisor ratings'
