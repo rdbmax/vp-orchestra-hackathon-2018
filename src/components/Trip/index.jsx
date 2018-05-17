9720801eb1879e25987996ece9f3a123f89b3b87
Marc Fagot <mfagot@vente-privee.com>
2018-05-17 14:20:13 +0200

    updated Trip

--- src/components/Trip/index.jsx
+++ src/components/Trip/index.jsx
@@ -27,6 +27,55 @@ const ratingImgs = ( stars ) => {
   />)
 }
 
+const listDepaturesCities = departures => {
+  const allDepartureCities = departures.reduce((allCities, departure) => [...allCities, ...departure.departureCities], []);
+  const listCities = [...new Set(allDepartureCities)];
+  let listCitiesString;
+
+  if (listCities.length <= 3) {
+    listCitiesString = listCities.reduce((str, city) => str + ", " + city, "");
+  } else {
+    const tmpStr = listCities
+      .slice(0, 3)
+      .reduce((str, city) => str + ", " + city, "");
+    listCitiesString = tmpStr + "...";
+  }
+
+  return listCitiesString.substr(2);
+};
+
+const listTripActivities = trip => {
+  let activitiesStr
+
+  if (trip.activities.length <= 3) {
+    activitiesStr = trip.activities.reduce((str, activity) => str + ", " + activity, "")
+  } else {
+    activitiesStr = trip.activities.slice(0, 3).reduce((str, activity) => str + ", " + activity, "")
+    activitiesStr += "..."
+  }
+
+  return activitiesStr.substr(2)
+};
+  
+const weatherLabel = departures => departures[0].climate
+
+const temperatureLabel = departures => {
+  return departures[0].temperature.toString() + " °C"
+}
+
+const saisonalityLabel = departures => {
+  return departures[0].saisonality + " saison"
+}
+
+const securityBlock = () => {
+  return (
+    <div>
+      <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/SecuriteRose.png`} />
+      <div> Pays sûr </div>
+    </div>
+  )
+}
+
 const Trip = ({ trip: { offerDescription: trip, departureDates: depatures }, classes }) => (
   <div className={classes.wrapper}>
     {trip.label} {ratingImgs(trip.stars)}
@@ -51,9 +100,26 @@ const Trip = ({ trip: { offerDescription: trip, departureDates: depatures }, cla
         {trip.description}
       </div>
       <div>
-        <img src={`${process.env.PUBLIC_URL}/img/filters/VilleDepartRose.png`} />
-        <div> { [...new Set(depatures.reduce((acc, curr) => acc + curr.departureCities, ''))] } </div>
+        <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/VilleDepartRose.png`} />
+        <div> { listDepaturesCities(depatures) } </div>
       </div>
+      <div>
+        <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/ActivitesRose.png`} />
+        <div> { listTripActivities(trip) } </div>
+      </div>
+      <div>
+        <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/ClimatRose.png`} />
+        <div> { weatherLabel(depatures) } </div>
+      </div>
+      <div>
+        <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/TemperatureRose.png`} />
+        <div> { temperatureLabel(depatures) } </div>
+      </div>
+      <div>
+        <img style={{ width: '20px' }} src={`${process.env.PUBLIC_URL}/img/filters/SaisonnaliteRose.png`} />
+        <div> { saisonalityLabel(depatures) } </div>
+      </div>
+      { trip.security && securityBlock()}
     </div>
   </div>
 )
